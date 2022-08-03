5a7e915f27b2153a45163645e9d8fbdfed543eb8
elena_s <esonina@icloud.com>
2022-08-03 23:06:46 +0300

    add solution for the problem about friends & dog

---
+++ Example_Dog/Program.cs
@@ -0,0 +1,27 @@
+﻿int count = 0;
+int distance = 10000;
+int FirstFriendSpeed = 1;
+int SecondFriendSpeed = 2;
+int DogSpeed = 5;
+int Friend = 2;
+int time;
+
+
+while (distance > 10)
+{
+    if (Friend == 1)
+    {
+        time = distance / (FirstFriendSpeed + DogSpeed);
+        Friend = 2; 
+    }
+    else
+    {
+        time = distance / (SecondFriendSpeed + DogSpeed);
+        Friend = 1;
+    }  
+    
+    distance = distance - (FirstFriendSpeed + SecondFriendSpeed) * time;
+    count++;
+} 
+
+Console.WriteLine($"Собака пробежит {count} раз");
