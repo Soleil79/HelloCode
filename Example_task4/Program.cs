a8f64d276e37841f1ca0832b3acc8b260d518747
elena_s <esonina@icloud.com>
2022-08-05 00:24:47 +0300

    add task4 int max of 3 solution

---
+++ Example_task4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа определения наибольшего целого числа из трех заданных.
+
+Console.Write("Введите первое целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье целое число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
