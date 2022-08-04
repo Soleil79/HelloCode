c0a3f57367c9948357efd33031c08cd34e19d5c5
elena_s <esonina@icloud.com>
2022-08-05 00:05:08 +0300

    add task2 int max&min solution

---
+++ Example_task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Программа определения наибольшего и наименьшего целого числа из двух заданных.
+int max;
+int min;
+
+Console.Write("Введите первое целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число: ");
+int b = Convert.ToInt32(Console.ReadLine());;
+
+if (a > b) 
+{
+    max = a; min = b;
+}
+else 
+{
+    max = b; min = a;
+}
+
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
