f99c317b665a910d31ce84a3a17246119a94a26b
elena_s <esonina@icloud.com>
2022-08-05 22:24:50 +0300

    add task6 облегченный вариант

---
+++ Example_task6_easy/Program.cs
@@ -0,0 +1,13 @@
+﻿//Программа для определения того, является ли заданное число четным или нечетным.
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine($"Да, {num} - четное число");
+}
+else
+{
+    Console.WriteLine($"Нет, {num} - нечетное число");
+}
