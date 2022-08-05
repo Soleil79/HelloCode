4a3b8a08fcc9ce57532adbb65dfda616855525ac
elena_s <esonina@icloud.com>
2022-08-05 15:16:23 +0300

    add Example_task6 even/odd numbers

---
+++ Example_task6/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Проверим является ли четным число (да/нет)?");
+String answer = Console.ReadLine();
+while (answer.ToLower() == "да")
+{
+    Console.WriteLine("Введите число");
+    int num = Convert.ToInt32(Console.ReadLine());
+
+    if (num % 2 == 0)
+    {
+        Console.WriteLine($"Да, {num} - четное число");
+    }
+    else
+    {
+        Console.WriteLine($"Нет, {num} - нечетное число");
+    }
+    Console.WriteLine("Проверим является ли четным число (да/нет)?");
+    String answer_no = Console.ReadLine();
+    if(answer_no.ToLower() == "нет") break;
+} 
