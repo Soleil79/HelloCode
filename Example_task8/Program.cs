d44985926f609cd26a02da6940f6e1953aaf40dc
elena_s <esonina@icloud.com>
2022-08-05 18:36:27 +0300

    add task8 all even/odd numbers in the given N

---
+++ Example_task8/Program.cs
@@ -0,0 +1,15 @@
+﻿//Программа определения всех четных чисел от 1 до заданного пользователем числа N.
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int I = N - (N - 1);
+
+while (I <= N)
+{
+    if (I % 2 == 0)
+    {
+        Console.WriteLine($"Четные числа: {I}");
+    }
+    I++; 
+} 
