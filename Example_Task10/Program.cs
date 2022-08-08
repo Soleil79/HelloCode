63ec87efb5dc22ccf3d5b1675d7579422551bf5a
elena_s <esonina@icloud.com>
2022-08-08 19:42:37 +0300

    add Example_task13 determine third digit in number

--- Example_Task10/Program.cs
+++ Example_Task10/Program.cs
@@ -1,19 +1,17 @@
-﻿Console.WriteLine ("Введите трехзначное число");
+﻿// 1 Вариант решения
+/*Console.WriteLine ("Введите трехзначное число");
 int num = Convert.ToInt32(Console.ReadLine());
 
 int a = num / 100; // первое число
 int b = num % 100 / 10; // второе число
 int c = num % 10; // третье число
 
-Console.WriteLine ($"Вторая цифра числа {num}: "  + b);
+Console.WriteLine ($"Вторая цифра числа {num}: "  + b);*/
 
 
-/*int a, b, c;
-            for (int i = 100; i < 1000; i++)
-            {
-                a = i % 10;//находим первую цифру, остатком от деления(%)
-                b = (i/10)%10;//находим среднюю цифру, остатком от деления(%) и целой частью от деления(/)
-                c = i / 100;//находим последнюю цифру
-                if (a + c == b) { Console.WriteLine(i); }//делаем проверку и выводим на экран
-            }
-*/
+// 2 Вариант
+Console.WriteLine ("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+string stringNum = Convert.ToString(num);
+
+Console.WriteLine("вторая цифра этого числа: " + stringNum[1]);
