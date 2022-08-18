f1296ac014d4ca54e62198a817b727f891d98438
elena_s <esonina@icloud.com>
2022-08-19 00:13:18 +0300

    add tasks 25,27,29-2

---
+++ Example_task27/Program.cs
@@ -0,0 +1,49 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+using System; 
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine ("Введите число");
+        string number = Convert.ToString(Console.ReadLine());
+        int [] array = new int[number.Length];
+        int sum = 0;
+        for (int i = 0; i < number.Length; i++)
+        {
+            array[i] = int.Parse(number[i].ToString());
+            sum = sum + array [i];
+        }
+        Console.WriteLine ($"Сумма цифр в числе {number} равна {sum}");
+        
+      
+    }  
+ 
+}
+
+
+
+/*Console.WriteLine($"\nЗадача 27. Выдаёт сумму цифр в числе");
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+  int SumNumber(int numberN){
+    
+    int counter = Convert.ToString(numberN).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++){
+      advance = numberN - numberN % 10;
+      result = result + (numberN - advance);
+      numberN = numberN / 10;
+    }
+   return result;
+  }
+
+int sumNumber = SumNumber(numberN);
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);*/
---
+++ Example_task29-2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+class program
+{
+    static void Main (string[] args)
+    {
+        int[] array = new int [0];
+        while (true)
+        {
+            Console.WriteLine("Введите ряд чисел для заполнения массива ");
+            string meaning = Console.ReadLine();
+            if (meaning != "end")
+            {
+                int number = Convert.ToInt32(meaning);
+                int[] newArray = array;
+                array = new int[array.Length + 1];
+                for(int i = 0; i < newArray.Length; i++)
+                {
+                    array[i] = newArray [i];
+                }
+                array[array.Length-1] = number;
+            }
+            else
+            {
+                break;
+            }
+        }
+        PrintArray(array);
+    }
+
+    static void PrintArray (int[] coll) // вывод массива на печать
+        {
+          int i = 0;
+          int N = coll.Length;
+          Console.Write("[");
+          
+          while (i < N-1)
+          {
+            Console.Write($"{coll[i]},");
+            i++;
+            if ( i == N-1) Console.Write($"{coll[i]}");
+          }        
+          Console.Write("]");
+        }
+}
