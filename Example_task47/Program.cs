eb2f6f5c912a31798fb87f1b24f2d36fe81cbf45
elena_s <esonina@icloud.com>
2022-08-27 00:25:39 +0300

    add task47 random matrix

---
+++ Example_task47/Program.cs
@@ -0,0 +1,55 @@
+﻿/*Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+using System;
+class Program
+{
+      static void Main ()
+  {
+    Console.WriteLine("Необходимо задать двумерный массив размера m x n ");
+    Console.Write("Введите m: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите n: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine($"m = {m}, n = {n}.");
+
+    double[,] RandomMatr = new double[m,n];
+    FillArray(RandomMatr);
+    PrintArray(RandomMatr);
+  }
+
+    static void FillArray (double [,] matr)
+    {
+        for (int i = 0; i < matr.GetLength(0); i++)
+        {
+            for (int j = 0; j < matr.GetLength(1); j++)
+            {
+                matr[i, j] = new Random().Next(-100,100)/10.0;
+
+            }
+            Console.WriteLine();
+        }
+    }
+    
+    static void PrintArray (double [,] matr)
+    {
+        for (int i = 0; i < matr.GetLength(0); i++)
+        {
+            for (int j = 0; j < matr.GetLength(1); j++)
+            {
+                double RoundNum = Math.Round(matr[i, j], 1);
+                Console.Write(RoundNum + " ");
+          
+            }
+            Console.WriteLine();
+        }
+    }
+    
+
+
+} 
